998153d65f022f497106ccd5cfbe5db37320ea0b
panos <panos@almende.org>
2015-04-14 14:39:13 +0200

    Added config file

--- src/pipeline/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/panos/Desktop/pcl/src/pipeline/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/panos/turtlebot/devel;/home/panos/kobuki/devel;/home/panos/rocon/devel;/opt/ros/indigo".split(';') if "/home/panos/turtlebot/devel;/home/panos/kobuki/devel;/home/panos/rocon/devel;/opt/ros/indigo" != "" else []
